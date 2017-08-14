d30f14a7a2745719dcb377d6a0a416a186b8e4d8
Aqueum <martin@aqueum.com>
2017-08-14 18:47:15 +0100

    Add basic provider, property & photo tables

--- models.py
+++ models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Provider(models.Model):
+    provider_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.question_text
+        return self.provider_name
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Property(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    property_name = models.CharField(max_length=200)
+    bedrooms = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.choice_text
+        return self.property_name
+
+
+class Photo(models.Model):
+    prop = models.ForeignKey(Property, on_delete=models.CASCADE)
+    photo_url = models.CharField(max_length=200)
+    photo_credit = models.CharField(max_length=200)
+    photo_description = models.CharField(max_length=800)
+
+    def __str__(self):
+        return self.photo_description
