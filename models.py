2ad13ef1522bf4a96e4ac8f1d7a3436dc1e39299
Aqueum <martin@aqueum.com>
2017-08-14 22:22:44 +0100

    Add property fields

--- models.py
+++ models.py
@@ -8,10 +8,41 @@ class Provider(models.Model):
         return self.provider_name
 
 
+class Council(models.Model):
+    council_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.council_name
+
+
+class PropetyType(models.Model):
+    property_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.property_type
+
+
+class Support(models.Model):
+    property_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.property_type
+
+
 class Property(models.Model):
     provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    property_type = models.ForeignKey(PropetyType)
     property_name = models.CharField(max_length=200)
     bedrooms = models.IntegerField(default=1)
+    beds = models.IntegerField(default=1)
+    max_occupants = models.IntegerField(default=1)
+    rent_pcm = models.DecimalField(decimal_places=2, default=0.00)
+    deposit = models.DecimalField(decimal_places=2, default=0.00)
+    parking = models.BooleanField
+    shared = models.BooleanField
+    support = models.ManyToManyField(Support)
+    wheelchair_accessible = models.BooleanField
+    council = models.ForeignKey(Council)
 
     def __str__(self):
         return self.property_name
