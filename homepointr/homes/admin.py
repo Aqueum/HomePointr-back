2e5a618720fcc460852321fafc369a30ff989595
Aqueum <martin@aqueum.com>
2017-08-15 23:52:59 +0100

    Add admin interface

--- homepointr/homes/admin.py
+++ homepointr/homes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bread
+
+
+class BreadAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Bread, BreadAdmin)
