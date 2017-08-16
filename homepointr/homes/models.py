939c83a44d45484d30e2123e5e28078e6c966cc6
Aqueum <martin@aqueum.com>
2017-08-16 10:17:08 +0100

    Try foreign key

--- homepointr/homes/models.py
+++ homepointr/homes/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class Grain(models.Model):
+    name = models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.name
+
+
 class Bread(models.Model):
     name = models.CharField(max_length=127)
-    grain = models.CharField(max_length=127, default='wheat')
+    grain = models.ForeignKey(Grain)
 
     def __str__(self):
         return self.name
