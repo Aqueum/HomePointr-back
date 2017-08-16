84d2c1726ed1c42e0e8d33e94398b51d0c3b4513
Aqueum <martin@aqueum.com>
2017-08-16 17:19:13 +0100

    Remove Grain foreignkey

--- homepointr/homes/models.py
+++ homepointr/homes/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 
 
-class Grain(models.Model):
-    name = models.CharField(max_length=127)
-
-    def __str__(self):
-        return self.name
-
-
 class Bread(models.Model):
     name = models.CharField(max_length=127)
-    grain = models.ForeignKey(Grain)
+    grain = models.CharField(max_length=127, default="Wheat")
 
     def __str__(self):
         return self.name
