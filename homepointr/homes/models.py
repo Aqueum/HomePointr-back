b8ad7352af4496f2ca615f8ae105b1a44437d7f6
Aqueum <martin@aqueum.com>
2017-08-16 17:29:50 +0100

    Add grain foreignkey

--- homepointr/homes/models.py
+++ homepointr/homes/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class Grain(models.Model):
+    name = models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.name
+
+
 class Bread(models.Model):
     name = models.CharField(max_length=127)
-    grain = models.CharField(max_length=127, default="Wheat")
+    grain = models.ForeignKey(Grain, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
