c3f67b41acb156c30053a6d51c8cce01c2026b83
Aqueum <martin@aqueum.com>
2017-08-16 09:55:09 +0100

    Add field

--- homepointr/homes/models.py
+++ homepointr/homes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Bread(models.Model):
     name = models.CharField(max_length=127)
+    grain = models.CharField(max_length=127)
 
     def __str__(self):
         return self.name
